1d7f037c77d91b8a914610932b6510f92ef05080
Administrator@MYDOMAIN.LK <Administrator@MYDOMAIN.LK>
2018-12-31 15:38:51 -0800

    Update Module1.vb

--- Module1.vb
+++ Module1.vb
@@ -10,6 +10,11 @@
         Dim b As Integer = Console.ReadLine()
         Dim c As Integer = a + b
         Console.WriteLine("Addition is :" + c.ToString())
+        a = Console.ReadLine()
+        Console.WriteLine("Enter B:")
+        b = Console.ReadLine()
+        c = a - b
+        Console.WriteLine("substraction is :" + c.ToString())
         Console.Read()
     End Sub
 
