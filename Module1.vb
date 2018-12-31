ecfb81964c3da1e5fa62de4a594e93f5b73e89c0
Administrator@MYDOMAIN.LK <Administrator@MYDOMAIN.LK>
2018-12-31 14:17:05 -0800

    Initial commit

---
+++ Module1.vb
@@ -0,0 +1,11 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hellow world...")
+        Console.WriteLine("updating...")
+        Console.WriteLine("updating...")
+        Console.WriteLine("Hellow world...")
+        Console.Read()
+    End Sub
+
+End Module
